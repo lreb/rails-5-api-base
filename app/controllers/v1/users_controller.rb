1f6042067e0fb9ab9f431f3e718d1017b9ea56ec
Luis Raul Espinoza Barboza - vm <respinozabarboza@gmail.com>
2017-07-06 14:23:24 -0600

    user controller

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,18 @@
+class V1::UsersController < ApplicationController
+  	def create
+  		puts user_params
+	  user = User.new(user_params)
+
+	  if user.save
+	    render json: {status: 'User created successfully'}, status: :created
+	  else
+	    render json: { errors: user.errors.full_messages }, status: :bad_request
+	  end
+	end
+
+	private
+
+	def user_params
+	  params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+end

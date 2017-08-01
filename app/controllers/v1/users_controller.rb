660646a46e0a89accbed1d3b45aecf64b8d4f40f
Luis Raul Espinoza Barboza - vm <respinozabarboza@gmail.com>
2017-08-01 16:21:30 -0600

    validated when the user is already in database

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -5,14 +5,17 @@ class V1::UsersController < ApplicationController
   		puts "params"
   		puts user_params[:email]
   		puts user_params[:password]
-  		
-	  user = User.new(user_params)
-	  puts user.to_json
-	  if user.save
-	    render json: {status: 'User created successfully'}, status: :created
-	  else
-	    render json: { errors: user.errors.full_messages }, status: :bad_request
-	  end
+  	  if((User.find_by email: user_params[:email]) != nil)
+  	  	render json: { status: 'The email is already in use' }, status: :bad_request
+  	  else
+  		user = User.new(user_params)
+		puts user.to_json
+		if user.save
+		  render json: {status: 'User created successfully'}, status: :created
+		else
+		  render json: { errors: user.errors.full_messages }, status: :bad_request
+		end
+  	  end
 	end
 
 	private

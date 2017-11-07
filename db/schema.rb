9dd396ebe0a20ea96c710b28858c870a98882b3e
Luis Raul Espinoza Barboza - vm <respinozabarboza@gmail.com>
2017-11-07 15:52:59 -0700

    updated users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107223302) do
+ActiveRecord::Schema.define(version: 20171107224442) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,9 @@ ActiveRecord::Schema.define(version: 20171107223302) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "image"
+    t.integer  "gender_id"
+    t.boolean  "deleted"
   end
 
 end

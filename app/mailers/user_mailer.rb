46022af71542bda4a8b699b1bba84d4bf5220fa6
Luis Raul Espinoza Barboza - vm <respinozabarboza@gmail.com>
2017-12-12 16:55:55 -0700

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
 	def registration_confirmation(user)
     @user = user
     @url  = # generate confirmation url
-    mail(to: @user.email, subject: "Confirm your Culttt Membership!")
+    mail(to: @user.email, subject: "Welcome!")
   end
 end
